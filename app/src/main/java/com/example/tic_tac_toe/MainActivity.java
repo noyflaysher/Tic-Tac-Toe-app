b9fe085a4a1504b39521e3a5edff8fa1b82e6ff0
Admin <Netamamann@gmail.com>
2022-11-29 14:38:32 +0200

    style app

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tic_tac_toe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*ImageView s1= findViewById(R.id.main_activity_s1);
+
+        s1.setOnClickListener(view->);*/
+
     }
 }
